3538353cfb0f13c136a28a6ce21504d3b2f0e425
gwajoon <hengguojun25@gmail.com>
2023-10-06 09:19:30 +0800

    Add serial number to start view

--- PatientVitals WatchKit Extension/StartView.swift
+++ PatientVitals WatchKit Extension/StartView.swift
@@ -12,21 +12,32 @@ struct StartView: View {
     @EnvironmentObject var workoutManager: WorkoutManager
     var workoutTypes: [HKWorkoutActivityType] = [.running]
     
+    @State private var serialNumber: String = ""
+    
     var body: some View {
-        List(workoutTypes) { workoutType in
-            NavigationLink(
-                workoutType.name,
-                destination: SessionPagingView(),
-                tag: workoutType,
-                selection: $workoutManager.selectedWorkout
-            ).padding(EdgeInsets(top: 15, leading: 5, bottom: 15, trailing: 5)
-            )
+        VStack {
+            Text("Device Serial Number: \(serialNumber)").font(.system(.title3, design: .rounded))
+            
+            List(workoutTypes) { workoutType in
+                NavigationLink(
+                    workoutType.name,
+                    destination: SessionPagingView(),
+                    tag: workoutType,
+                    selection: $workoutManager.selectedWorkout
+                ).padding(EdgeInsets(top: 15, leading: 5, bottom: 15, trailing: 5)
+                )
+            }
+            .listStyle(.carousel)
+            .navigationBarTitle("Vitals")
+            .onAppear {
+                workoutManager.requestAuthorisation()
+                
+                if let simulatorUUID = ProcessInfo.processInfo.environment["SIMULATOR_UDID"] {
+                   self.serialNumber = simulatorUUID
+               }
+            }
         }
-        .listStyle(.carousel)
-        .navigationBarTitle("Workouts")
-        .onAppear {
-            workoutManager.requestAuthorisation()
-        }
+        
     }
 }
 
